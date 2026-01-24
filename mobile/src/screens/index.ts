77c5325e5f43cbbf5cdc1541130a917434e08b19
AWT_ Poke <jwang769@ucsc.edu>
2026-01-23 16:54:06 -0800

    gitignore

--- mobile/src/screens/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as LoginScreen } from './LoginScreen';
-export { default as RegisterScreen } from './RegisterScreen';
-export { default as HomeScreen } from './HomeScreen';
-export { default as SearchScreen } from './SearchScreen';
-export { default as MapScreen } from './MapScreen';
-export { default as ProfileScreen } from './ProfileScreen';
--- mobile/src/services/index.ts
+++
@@ -1,4 +0,0 @@
-export { authService } from './authService';
-export { apiService } from './apiService';
-export { currencyService } from './currencyService';
-export { mapService } from './mapService';
